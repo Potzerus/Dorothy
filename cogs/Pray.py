dd70bce032c03dcd89d1d5a7b361206eac1cc021
potzerus <Potzerus@gmail.com>
2021-03-17 19:13:23 +0100

    upgrades people upgrades

--- cogs/Pray.py
+++ cogs/Pray.py
@@ -12,7 +12,7 @@ class PrayCog(Cog):
     def has_char(self, user_id):
         return user_id in self.players
 
-    # replace this with an actual id system
+    # TODO:[after 1.0] replace this with an actual id system
     def encounter_id(self, user1_id, user2_id):
         return str(user1_id) + str(user2_id) if user1_id < user2_id else str(user2_id) + str(user1_id)
 
@@ -34,28 +34,40 @@ class PrayCog(Cog):
             "name": ctx.author.name,
             "max_health": 20,
             "health": 20,
-            "inventory": []
+            "inventory": [],
+            "level": 1,
+            "maxlevel": 20,
+            "titles": [],
         }
 
     @commands.command(name="duel")
     async def initiate_encounter(self, ctx, target: discord.Member):
-        if not self.has_char(ctx.author.id):
+        initiator = ctx.author.id
+        targeted = target.id
+        if not self.has_char(initiator):
             await ctx.send("You don't have a character!")
             return
-        if ctx.author.id == target.id:
+        if initiator == targeted:
             await ctx.send("You can't fight yourself!")
             return
-        if not self.has_char(target.id):
+        if not self.has_char(targeted):
             await ctx.send("Target doesn't have a character!")
             return
-        e_id = self.encounter_id(ctx.author.id, target.id)
+        e_id = self.encounter_id(initiator, targeted)
         # replace this with a check pulling up encounters and seeing if the characters are in any
-        if e_id in self.encounters:
-            await ctx.send("That encounter is already running")
-            return
+        for encounter in self.encounters:
+            if initiator in encounter["participants"]:
+                await ctx.send("You are already in combat!")
+                return
+            if targeted in encounter["participants"]:
+                await ctx.send("This character is already in combat!")
+                return
+
         encounter = {
-            "participants": [ctx.author.id, target.id],
+            "participants": [initiator, targeted],
             "turn_tracker": 0,
+            "turn_available": [True, True],
+            "id": e_id,
         }
 
     @commands.command()
