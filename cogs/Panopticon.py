62fc8dd95e4ac680dbafda2a4c420b3c94d434ff
Potzerus <Potzerus@gmail.com>
2021-05-15 16:21:27 +0200

    Fix fix fix

--- cogs/Panopticon.py
+++ cogs/Panopticon.py
@@ -13,8 +13,8 @@ class Panopticon(commands.Cog):
     async def on_message(self, message):
         if message.content == "!updog":
             if message.author.id in [122739797646245899, 125660719323676672]:
-                global tokens
-                tokens += 1
+
+                self.tokens += 1
                 await self.bot.get_guild(303307934774067210).get_channel(842931370695196682).send(
                     "Restart Initiated")
 
@@ -26,9 +26,8 @@ class Panopticon(commands.Cog):
         if after.status != discord.Status.offline:
             return
 
-        global tokens
-        if tokens > 0:
-            tokens -= 1
+        if self.tokens > 0:
+            self.tokens -= 1
             return
 
         await self.bot.get_guild(303307934774067210).get_channel(842931370695196682).send(
