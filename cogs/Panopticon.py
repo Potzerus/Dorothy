53691ae6fc238ae5fe5b2893eaf62b6099787d95
Potzerus <Potzerus@gmail.com>
2021-05-16 20:48:00 +0200

    Rework Intent in Panopticon

--- cogs/Panopticon.py
+++ cogs/Panopticon.py
@@ -8,7 +8,7 @@ class Panopticon(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        self.tokens = 0
+        self.intent = False
         self.time = None
 
     @commands.Cog.listener()
@@ -28,13 +28,23 @@ class Panopticon(commands.Cog):
         if after.Status != discord.Status.offline:
             return
 
-        if self.tokens > 0:
-            self.tokens -= 1
+        if self.intent:
+            self.intent = False
             return
 
         await self.bot.get_guild(303307934774067210).get_channel(842931370695196682).send(
             "Bot just went offline <@!122739797646245899>,<@!125660719323676672>")
 
-    @commands.command()
-    async def void_tokens(self):
-        self.tokens = 0
+    @commands.group()
+    async def intent(self, ctx):
+        await ctx.send(self.intent)
+
+    @intent.command()
+    async def off(self, ctx):
+        self.intent = False
+        await ctx.send(self.intent)
+
+    @intent.command()
+    async def on(self, ctx):
+        self.intent = True
+        await ctx.send(self.intent)
